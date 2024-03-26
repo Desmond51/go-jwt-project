eb5467948d7bb2a9815ea90dc4ac00507eb0aa0b
Desmond51 <desmondtekum9@gmail.com>
2024-03-26 17:21:42 +0100

    Added user controllers

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,3 +1,14 @@
 package routes
 
-func user
+import (
+	controllers "github.com/Desmond51/go-jwt-project/controllers"
+	"github.com/Desmond51/go-jwt-project/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controllers.GetUser())
+	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
+
+}
